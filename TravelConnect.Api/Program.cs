b1f88cda08b9c3fda25e2babb5ad01a11e56965d
Fabian Ramirez <fabianramirez0072@gmail.com>
2024-12-12 15:13:14 -0500

    Load infrastructure into Api

--- TravelConnect.Api/Program.cs
+++ TravelConnect.Api/Program.cs
@@ -1,25 +1,13 @@
+using TravelConnect.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var services = builder.Services;
+var config = builder.Configuration;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddInfrastructure(config);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.UseInfrastructure(app.Environment);
 app.Run();
