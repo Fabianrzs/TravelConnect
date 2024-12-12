c3e504072934e743380bf359658836d7a579b2ee
Fabian Ramirez <fabianramirez0072@gmail.com>
2024-12-12 01:08:37 -0500

    Include Adapters into infrastructure

---
+++ TravelConnect.Infrastructure/Adapters/Security/EncryptionService.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using TravelConnect.Domain.Ports;
+
+namespace TravelConnect.Infrastructure.Adapters.Security;
+
+public class EncryptionService(string encryptionKey) : IEncryptionService
+{
+    private readonly string _encryptionKey = encryptionKey ?? throw new ArgumentNullException(nameof(encryptionKey));
+
+    public string Encrypt(string plainText)
+    {
+        using var aes = Aes.Create();
+        var key = Encoding.UTF8.GetBytes(_encryptionKey.PadRight(32));
+        aes.Key = key;
+        aes.IV = new byte[16]; // Vector de inicialización en cero
+
+        using var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+        using var ms = new MemoryStream();
+        using var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+        using (var sw = new StreamWriter(cs))
+        {
+            sw.Write(plainText);
+        }
+
+        return Convert.ToBase64String(ms.ToArray());
+    }
+
+    public string Decrypt(string cipherText)
+    {
+        using var aes = Aes.Create();
+        var key = Encoding.UTF8.GetBytes(_encryptionKey.PadRight(32));
+        aes.Key = key;
+        aes.IV = new byte[16];
+
+        using var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+        using var ms = new MemoryStream(Convert.FromBase64String(cipherText));
+        using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+        using var sr = new StreamReader(cs);
+
+        return sr.ReadToEnd();
+    }
+}
---
+++ TravelConnect.Infrastructure/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TravelConnect.Infrastructure.Extensions;
+
+namespace TravelConnect.Infrastructure;
+
+public static class Startup
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddPesistence(config);
+        services.AddDomainServices();
+        services.AddJwtSettings(config);
+        services.AddMediator();
+        services.AddMapper();
+        services.AddValidator();
+        services.AddSwagger();
+        services.AddCorsPolicy();
+        services.AddControllers();
+    }
+
+    public static void UseInfrastructure(this WebApplication app, IWebHostEnvironment env)
+    {
+        app.UseCorsPolicy();
+        app.UseSwaggers(env);
+        app.UseExceptionMiddleware();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseHttpsRedirection();
+        app.MapControllers();
+    }
+}
