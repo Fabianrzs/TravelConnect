9eb98a35a753802fd6843f493e5f322be2c3b8fa
Fabian Ramirez <fabianramirez0072@gmail.com>
2024-12-12 14:24:23 -0500

    Include Extensions

---
+++ TravelConnect.Infrastructure/Extensions/PersistenceExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TravelConnect.Domain.Ports.Persistence;
+using TravelConnect.Infrastructure.Adapters.Persistence.DbContexts;
+using TravelConnect.Infrastructure.Adapters.Persistence.Repositories;
+
+namespace TravelConnect.Infrastructure.Extensions;
+
+public static class PersistenceExtension
+{
+    public static IServiceCollection AddPesistenceServices(this IServiceCollection svc, IConfiguration config)
+    {
+        var stringConnection = config.GetConnectionString("DefaultConnection");
+        svc.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(stringConnection));
+
+        svc.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+        return svc;
+    }
+}
